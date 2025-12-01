cd1bb225f9de589562edc0215e522b9a6a39d6da
NandoBlack <hernando.perez@est.iudigital.edu.co>
2025-12-01 08:03:17 -0500

    Se eliminaron archivos vacíos y se agregó el archivo creditsData.js con datos de productos crediticios.

---
+++ src/data/creditsData.js
@@ -0,0 +1,47 @@
+export const productos = [
+{
+    id: 'libre-inversion',
+    tipo: 'libre',
+    nombre: 'Crédito Libre Inversión',
+    tasa: '12.5% E.A.',
+    montoDesde: '$1.000.000',
+    montoHasta: '$100.000.000',
+    plazoMax: '60 meses'
+},
+{
+    id: 'vehiculo',
+    tipo: 'vehiculo',
+    nombre: 'Crédito Vehículo',
+    tasa: '9.8% E.A.',
+    montoDesde: '$5.000.000',
+    montoHasta: '$200.000.000',
+    plazoMax: '72 meses'
+},
+{
+    id: 'vivienda',
+    tipo: 'vivienda',
+    nombre: 'Crédito Vivienda',
+    tasa: '7.2% E.A.',
+    montoDesde: '$20.000.000',
+    montoHasta: '$500.000.000',
+    plazoMax: '240 meses'
+},
+{
+id: 'educativo',
+    tipo: 'educativo',
+    nombre: 'Crédito Educativo',
+    tasa: '6.5% E.A.',
+    montoDesde: '$500.000',
+    montoHasta: '$50.000.000',
+    plazoMax: '120 meses'
+},
+{
+    id: 'empresarial',
+    tipo: 'empresarial',
+    nombre: 'Crédito Empresarial',
+    tasa: '11.0% E.A.',
+    montoDesde: '$10.000.000',
+    montoHasta: '$1.000.000.000',
+    plazoMax: '180 meses'
+}
+];
