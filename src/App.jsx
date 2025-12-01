8540e6ba2e8b58fa0e8c8563f9e2113030732d8b
NandoBlack <hernando.perez@est.iudigital.edu.co>
2025-12-01 08:18:34 -0500

    Se actualizó package.json y package-lock.json para incluir react-router-dom como dependencia. Se reestructuró el componente App para implementar rutas básicas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-// src/App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-// ... Importa Simulador y Solicitar
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar /> {/* Componente Reutilizable  */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/simulador" element={/* Componente Simulador */} />
-        <Route path="/solicitar" element={/* Componente Solicitar */} />
+        <Route path="/" element={<h1>Inicio</h1>} />
+        <Route path="/productos" element={<h1>Productos</h1>} />
+        <Route path="/nosotros" element={<h1>Nosotros</h1>} />
+        <Route path="/contacto" element={<h1>Contacto</h1>} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
-export default App;
+
+export default App
